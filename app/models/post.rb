05d16f7b9cf2a9d8db2704f412015212f1309e65
dominic lizarraga <dominiclizarraga@hotmail.com>
2022-08-03 23:36:47 -0700

    add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
   validates :title, :image, :description, presence: true
 
+  validates :title, length: { minimum: 6 }
+  validates :description, length: { minimum: 15 }
+
   has_one_attached :image
 end
